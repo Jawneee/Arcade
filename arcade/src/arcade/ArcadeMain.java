fdd0afd5fcf7d1f7ec12a5fea546a8863a209913
johna <johna@DESKTOP-SNCAS9I>
2022-10-28 21:38:42 -0500

    Hello there

---
+++ arcade/src/arcade/ArcadeMain.java
@@ -0,0 +1,10 @@
+package arcade;
+
+public class ArcadeMain {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
---
+++ arcade/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author johna
+ *
+ */
+module arcade {
+}
